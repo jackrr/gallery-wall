306d754693a4826cce17ed42006dfe48cdcb533f
Jack <jack.ratner@urbint.com>
2020-07-28 08:48:09 -0400

    add more images

--- main.js
+++ main.js
@@ -17,24 +17,48 @@ ctx.fillStyle = 'white'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 const images = [{
+  hide: true,
   src: 'skull-high-res-03.jpg',
-  width: 9,
-  height: 6,
-  top: 16,
+  width: 11,
+  height: 7.33,
+  top: 14,
   left: 41
 }, {
   src: 'plant-still-second-high-res-01.jpg',
   width: 8.25,
   height: 11.75,
-  top: 24,
+  top: 23,
   left: 41
 },
 {
   src: 'hand.jpg',
   width: 9,
   height: 12,
-  top: 10,
+  top: 11,
   left: 30
+},
+{
+  src: 'arizona.jpg',
+  width: 14,
+  height: 11,
+  top: 24,
+  left: 25
+},
+{
+  src: 'worker.jpg',
+  width: 11,
+  height: 7.33,
+  top: 14,
+  left: 41
+  // top: 23,
+  // left: 51
+},
+{
+  src: 'terminal.jpg',
+  width: 11,
+  height: 7.333,
+  top: 15.66,
+  left: 17
 }]
 
 const renderImage = (ctx, meta) => {
@@ -45,6 +69,7 @@ const renderImage = (ctx, meta) => {
 }
 
 images.forEach(meta => {
+  if (meta.hide) return;
   img = new Image(pixelInchRatio * meta.width, pixelInchRatio * meta.height)
   img.src = meta.src
   img.onload = renderImage(ctx, meta)
