3955d37dfa1f7fc5d80d9ccecdfcd5b4606ff0b9
Jack Ratner <jackratner@gmail.com>
2020-07-28 21:44:58 -0400

    weights and measures

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 
 const galleryWidth = 90
 const galleryHeight = 48
-const pixelInchRatio = 14
+const pixelInchRatio = 15
+
+const frameSize = 0.5
+const matteSize = 1
+const gapSize = 0.5
 
 const width = galleryWidth * pixelInchRatio
 const height = galleryHeight * pixelInchRatio
@@ -16,62 +20,130 @@ canvas.height = height
 ctx.fillStyle = 'white'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-const images = [{
-  hide: true,
-  src: 'skull-high-res-03.jpg',
-  width: 11,
-  height: 7.33,
-  top: 14,
-  left: 41
-}, {
+const images = [
+{
   src: 'plant-still-second-high-res-01.jpg',
-  width: 8.25,
-  height: 11.75,
-  top: 23,
-  left: 41
+  width: 10,
+  height: 15,
+  top: 24,
+  left: 40,
+  hOrder: 2,
+  vOrder: 1
 },
 {
   src: 'hand.jpg',
   width: 9,
   height: 12,
   top: 11,
-  left: 30
+  left: 30,
+  hOrder: 1
 },
 {
   src: 'arizona.jpg',
   width: 14,
   height: 11,
   top: 24,
-  left: 25
+  left: 25,
+  hOrder: 1,
+  vOrder: 1,
 },
 {
   src: 'worker.jpg',
-  width: 11,
-  height: 7.33,
-  top: 14,
-  left: 41
-  // top: 23,
-  // left: 51
+  width: 15,
+  height: 10,
+  top: 13,
+  left: 40,
+  hOrder: 2
 },
 {
   src: 'terminal.jpg',
-  width: 11,
-  height: 7.333,
-  top: 15.66,
-  left: 17
+  width: 15,
+  height: 10,
+  top: 13,
+  left: 14
+},
+{
+  name: "rectangle",
+  color: "#b01985",
+  width: 9,
+  height: 12,
+  left: 51,
+  top: 24,
+  hOrder: 3,
+  vOrder: 1
+},
+{
+  name: "square",
+  color: "#b01985",
+  width: 9,
+  height: 9,
+  left: 15,
+  top: 24,
+  vOrder: 1
 }]
+
+const getTop = meta => {
+  return meta.top  + ((meta.vOrder || 0) * ((2 * matteSize) + (2 * frameSize) + gapSize))
+}
+
+const getLeft = meta => {
+  return meta.left  + ((meta.hOrder || 0) * ((2 * matteSize) + (2 * frameSize) + gapSize))
+}
 
 const renderImage = (ctx, meta) => {
   // use function syntax to preserve "this" binding
   return function() {
-    ctx.drawImage(this, pixelInchRatio * meta.left, pixelInchRatio * meta.top, this.width, this.height)
+    ctx.drawImage(this, pixelInchRatio * getLeft(meta), pixelInchRatio * getTop(meta), this.width, this.height)
   }
 }
 
+const drawFrame = (ctx, meta) => {
+  ctx.lineWidth = pixelInchRatio * frameSize
+  ctx.strokeStyle = "#fcd2a4"
+  // ctx.lineWidth = 2
+  ctx.strokeRect(
+    pixelInchRatio * (getLeft(meta) - ((0.5 * frameSize) + matteSize)),
+    pixelInchRatio * (getTop(meta) - ((0.5 * frameSize) + matteSize)),
+    pixelInchRatio * (meta.width + (2 * matteSize) + frameSize),
+    pixelInchRatio * (meta.height + (2 * matteSize) + frameSize)
+  )
+}
+
+const drawMatte = (ctx, meta) => {
+  // noop for now
+}
+
+let xMin = 10000
+let xMax = 0
+let yMin = 10000
+let yMax = 0
+
 images.forEach(meta => {
   if (meta.hide) return;
-  img = new Image(pixelInchRatio * meta.width, pixelInchRatio * meta.height)
-  img.src = meta.src
-  img.onload = renderImage(ctx, meta)
-  return img
+
+  drawFrame(ctx, meta)
+  drawMatte(ctx, meta)
+
+  const left = getLeft(meta)
+  const width = meta.width + (2 * frameSize) + (2 * matteSize)
+  const top = getTop(meta)
+  const height = meta.height + (2 * frameSize) + (2 * matteSize)
+
+  if (left < xMin) xMin = left
+  if ((width + left) > xMax) xMax = width + left
+  if (top < yMin) yMin = top
+  if ((height + top) > yMax) yMax = height + top
+
+
+  if (meta.color) {
+    ctx.fillStyle = meta.color
+    ctx.fillRect(pixelInchRatio * getLeft(meta), pixelInchRatio * getTop(meta), pixelInchRatio * meta.width, pixelInchRatio * meta.height)
+  } else {
+    img = new Image(pixelInchRatio * meta.width, pixelInchRatio * meta.height)
+    img.src = meta.src
+    img.onload = renderImage(ctx, meta)
+  }
 })
+
+console.log({xMin, xMax, yMin, yMax})
+console.log({width: xMax - xMin, height: yMax - yMin})
